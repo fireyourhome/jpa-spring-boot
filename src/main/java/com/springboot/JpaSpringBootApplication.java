e0e9e6c8cf515b4476e863eded1d7266239be6f2
fireyourhome <786876474@qq.com>
2018-06-14 18:19:23 +0800

    拦截器，过滤器

--- src/main/java/com/springboot/JpaSpringBootApplication.java
+++ src/main/java/com/springboot/JpaSpringBootApplication.java
@@ -1,13 +1,26 @@
 package com.springboot;
 
+import com.springboot.servlet.MyServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-
+/*@ServletComponentScan*/
 public class JpaSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaSpringBootApplication.class, args);
     }
+
+    /**
+     * 使用代码注册Servlet（不需要@ServletComponentScan注解）
+     * @return
+     */
+    /*@Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        return new ServletRegistrationBean(new MyServlet(), "/look/*");// ServletName默认值为首字母小写，即myServlet
+    }*/
 }
